2294252290b3f8bc3d69eb66e907725421ef7b7d
Robin Stolpe <robin@stolpe.io>
2022-01-29 15:10:51 +0100

    Added Get-VMSerial

---
+++ VMWare/vSphere/Get-VMSerial.ps1
@@ -0,0 +1,23 @@
+﻿<# This Script get's the BIOS serialnumber from a VM and reports it back to the variable $VMName.
+It also fromates it in the correct way. #>
+
+function Get-VMSerial {
+    param(
+        [Parameter(Mandatory)][string]$VMName,
+        [Parameter(Mandatory)][string]$VIServer,
+        [Parameter(Mandatory)][string]$VIUsername,
+        [Parameter(Mandatory)][SecureString]$VIPassword
+    )
+    Connect-VIServer -Server $VIServer -User $VIUsername -Password $VIPassword
+
+    $s = ((Get-VM -Name $VMName).ExtensionData.Config.Uuid).Replace("-", "")
+    $Uuid = "VMware-"
+    for ($i = 0; $i -lt $s.Length; $i += 2) {
+        $Uuid += ("{0:x2}" -f [byte]("0x" + $s.Substring($i, 2)))
+        if ($Uuid.Length -eq 30) { $Uuid += "-" } else { $Uuid += " " }
+    }
+
+    $Uuid.TrimEnd()
+
+    Disconnect-VIServer -Server $VIServer -Force -Confirm:$false
+}
